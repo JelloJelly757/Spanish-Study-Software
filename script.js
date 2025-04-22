1c358c4dcb24f5922be35211f173d406455b02ef
Elizabeth <elizabethg2796@tuckahoeschools.org>
2025-04-22 02:13:24 +0000

    so little commits bc i transferred this from a diff repo!

---
+++ script.js
@@ -0,0 +1,62 @@
+let questionList = ['the apple', 'the man', 'the women', 'the friend (masculine)', 'the game', 'the aiport', 'the beach', "the cat", 'the dog', 'the house']; 
+let answerList = ['la manzana', 'el hombre', 'la mujer', 'el amigo', 'el partido', 'el aerpuerto', 'la playa', 'el gato', 'el perro', 'la casa']; 
+let score = 0; 
+let questionIndex = 0; 
+
+const question = document.querySelector('.questioncon'); 
+const button1 = document.getElementById('button1'); 
+const button2 = document.getElementById('button2'); 
+const input = document.querySelector('#input-field'); 
+let scoreContainer = document.querySelector('.scorecon');
+let summaryContainer = document.querySelector('.summarycon')
+
+question.textContent = questionList[questionIndex];
+
+let submitFunction = () =>{
+    button1.addEventListener('click', () =>{
+        if(input.value === answerList[questionIndex]){
+            questionIndex += 1; 
+            score += 1; 
+            question.textContent = questionList[questionIndex]; 
+            scoreContainer.textContent = "Score: " + score; 
+        }else{
+            score -= 1;  
+            scoreContainer.textContent = "Score: " + score;
+        }
+
+        //PUT ENDGAME FUNCTION HERE
+        endGame();
+    });
+}
+
+let endGame = () =>{
+    if(questionIndex > 9){
+        let summaryHTML = "<h1>Game Summary:</h1><ul>";
+        for (let i = 0; i < questionList.length; i++) {
+            summaryHTML += `<li>Word In English: ${questionList[i]}<br>Translation: ${answerList[i]}</li>`;
+        }
+        summaryHTML += "</ul>";
+
+        summaryContainer.innerHTML = summaryHTML;
+
+        scoreContainer.textContent = "Final Score: " + score; 
+        question.textContent = "Congratulations, play again!"; 
+        input.style.display = "none"; 
+        button1.style.display = "none"; 
+        button2.style.display = "none";
+    }
+}
+
+submitFunction(); 
+
+let skipFunction = () =>{
+    button2.addEventListener('click', () =>{
+        questionIndex += 1;  
+            question.textContent = questionList[questionIndex];
+        });
+    }
+
+skipFunction();
+
+//need to add comments
+//need to add parameter to my endgame function
